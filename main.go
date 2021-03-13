9c3c7eced149eb6442002953a7f4eacaf18c9a5d
Ismael Garcia <igarcia@illumina.com>
2021-03-12 21:15:09 -0800

    Add certificate scanner

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
+package main
 
+import (
+	"crypto/tls"
+	"fmt"
+	"time"
+)
+
+func main() {
+	conn, err := tls.Dial("tcp", "freecodecamp.org:443", nil)
+	if err != nil {
+		panic("Server does not support SSL certificate err: " + err.Error())
+	}
+
+	err = conn.VerifyHostname("freecodecamp.org")
+	if err != nil {
+		panic("Hostname does not match with certificate: " + err.Error())
+	}
+	expiry := conn.ConnectionState().PeerCertificates[0].NotAfter
+	fmt.Printf("Issuer: %s\nExpiry: %v\n", conn.ConnectionState().PeerCertificates[0].Issuer, expiry.Format(time.RFC850))
+}
